bdc2e8131106b08ac15f779c2769c66f2f76e308
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-10-02 20:18:38 +0200

    feat: config updates, added hlchunk & supertab

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,21 @@
 --
 vim.opt.wrap = true
 vim.opt.textwidth = 80
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "jose-elias-alvarez/typescript.nvim",
+      init = function()
+        require("lazyvim.util").lsp.on_attach(function(_, buffer)
+          -- stylua: ignore
+          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+        end)
+      end,
+    },
+  },
+  -- use mini.starter instead of alpha
+  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+}
